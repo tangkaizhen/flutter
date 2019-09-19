e969cfde7986e681ffd3617295a66ba2969493f1
tangkaizhen <tangkaizhen@longshine.com>
2019-09-19 13:57:49 +0800

    dart数据类型

---
+++ demo1/lession copy.dart
@@ -0,0 +1,12 @@
+// 在dart里面，所有需要执行的代码，必须放到main方法里面
+/**
+ * dart里面有类型校验
+ * 
+ * dart 常量  final和const  区别在于：final可以开始不赋值，final不仅仅有const编译时常量的特性
+ * 最重要是运行时常量，即运行时第一次使用才初始化
+ * 
+ */
+main(){
+  var str='hello world';
+  print(str);
+}
